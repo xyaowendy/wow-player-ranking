6c4c3b45b70f247f652d5b81dff0518e450ed465
Wendy Jiang <wendyjiang0611@gmail.com>
2022-09-23 12:02:11 +1000

    Modified UI for wow data, no edit and delete is needed. #1

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import { Route, Routes } from "react-router-dom";
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
 
 const App = () => {
     return (
